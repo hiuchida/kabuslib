04efda9ecbb5645d3c2c37a67c8025c7cd2ca043
hiuchida <hiro4uchida@gmail.com>
2022-12-19 16:14:46 +0900

    enum SecurityTypeCode

---
+++ src/main/java/com/github/hiuchida/api/consts/SecurityTypeCode.java
@@ -0,0 +1,31 @@
+package com.github.hiuchida.api.consts;
+
+public enum SecurityTypeCode {
+	指数(0), 現物(1), 日経225先物(101), 日経225OP(103), TOPIX先物(107), JPX400先物(121), NYダウ(144), 日経平均VI(145), 東証マザーズ指数先物(154),
+	TOPIX_REIT(155), TOPIX_CORE30(171), 日経平均225ミニ先物(901), TOPIXミニ先物(907);
+
+	private int id;
+
+	private SecurityTypeCode(int id) {
+		this.id = id;
+	}
+
+	public int intValue() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return Integer.toString(id);
+	}
+
+	public static SecurityTypeCode valueOf(int id) {
+		for (SecurityTypeCode e : values()) {
+			if (e.id == id) {
+				return e;
+			}
+		}
+		throw new IllegalArgumentException("No enum constant id=" + id);
+	}
+
+}
---
+++ src/test/java/com/github/hiuchida/api/consts/SecurityTypeCodeTest.java
@@ -0,0 +1,60 @@
+package com.github.hiuchida.api.consts;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SecurityTypeCodeTest {
+
+	@Test
+	public void intValueTest() {
+		assertEquals(0, SecurityTypeCode.指数.intValue());
+		assertEquals(1, SecurityTypeCode.現物.intValue());
+		assertEquals(101, SecurityTypeCode.日経225先物.intValue());
+		assertEquals(103, SecurityTypeCode.日経225OP.intValue());
+		assertEquals(107, SecurityTypeCode.TOPIX先物.intValue());
+		assertEquals(121, SecurityTypeCode.JPX400先物.intValue());
+		assertEquals(144, SecurityTypeCode.NYダウ.intValue());
+		assertEquals(145, SecurityTypeCode.日経平均VI.intValue());
+		assertEquals(154, SecurityTypeCode.東証マザーズ指数先物.intValue());
+		assertEquals(155, SecurityTypeCode.TOPIX_REIT.intValue());
+		assertEquals(171, SecurityTypeCode.TOPIX_CORE30.intValue());
+		assertEquals(901, SecurityTypeCode.日経平均225ミニ先物.intValue());
+		assertEquals(907, SecurityTypeCode.TOPIXミニ先物.intValue());
+	}
+
+	@Test
+	public void toStringTest() {
+		assertEquals("0", SecurityTypeCode.指数.toString());
+		assertEquals("1", SecurityTypeCode.現物.toString());
+		assertEquals("101", SecurityTypeCode.日経225先物.toString());
+		assertEquals("103", SecurityTypeCode.日経225OP.toString());
+		assertEquals("107", SecurityTypeCode.TOPIX先物.toString());
+		assertEquals("121", SecurityTypeCode.JPX400先物.toString());
+		assertEquals("144", SecurityTypeCode.NYダウ.toString());
+		assertEquals("145", SecurityTypeCode.日経平均VI.toString());
+		assertEquals("154", SecurityTypeCode.東証マザーズ指数先物.toString());
+		assertEquals("155", SecurityTypeCode.TOPIX_REIT.toString());
+		assertEquals("171", SecurityTypeCode.TOPIX_CORE30.toString());
+		assertEquals("901", SecurityTypeCode.日経平均225ミニ先物.toString());
+		assertEquals("907", SecurityTypeCode.TOPIXミニ先物.toString());
+	}
+
+	@Test
+	public void valueOfTest() {
+		assertEquals(SecurityTypeCode.指数, SecurityTypeCode.valueOf(0));
+		assertEquals(SecurityTypeCode.現物, SecurityTypeCode.valueOf(1));
+		assertEquals(SecurityTypeCode.日経225先物, SecurityTypeCode.valueOf(101));
+		assertEquals(SecurityTypeCode.日経225OP, SecurityTypeCode.valueOf(103));
+		assertEquals(SecurityTypeCode.TOPIX先物, SecurityTypeCode.valueOf(107));
+		assertEquals(SecurityTypeCode.JPX400先物, SecurityTypeCode.valueOf(121));
+		assertEquals(SecurityTypeCode.NYダウ, SecurityTypeCode.valueOf(144));
+		assertEquals(SecurityTypeCode.日経平均VI, SecurityTypeCode.valueOf(145));
+		assertEquals(SecurityTypeCode.東証マザーズ指数先物, SecurityTypeCode.valueOf(154));
+		assertEquals(SecurityTypeCode.TOPIX_REIT, SecurityTypeCode.valueOf(155));
+		assertEquals(SecurityTypeCode.TOPIX_CORE30, SecurityTypeCode.valueOf(171));
+		assertEquals(SecurityTypeCode.日経平均225ミニ先物, SecurityTypeCode.valueOf(901));
+		assertEquals(SecurityTypeCode.TOPIXミニ先物, SecurityTypeCode.valueOf(907));
+	}
+
+}
