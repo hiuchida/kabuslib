fe5a00efa32f1cc0d87afa38ef3daed3d9d3b938
hiuchida <hiro4uchida@gmail.com>
2022-12-25 17:58:05 +0900

    enum OrdTypeCode

---
+++ src/main/java/com/github/hiuchida/api/consts/OrdTypeCode.java
@@ -0,0 +1,35 @@
+package com.github.hiuchida.api.consts;
+
+/**
+ * 執行条件コード。
+ * 
+ * @see io.swagger.client.model.OrdersSuccess.ordType:注文約定情報／執行条件。
+ */
+public enum OrdTypeCode {
+	ザラバ(1), 寄り(2), 引け(3), 不成(4), 対当指値(5), IOC(6);
+
+	private int id;
+
+	private OrdTypeCode(int id) {
+		this.id = id;
+	}
+
+	public int intValue() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return Integer.toString(id);
+	}
+
+	public static OrdTypeCode valueOf(int id) {
+		for (OrdTypeCode e : values()) {
+			if (e.id == id) {
+				return e;
+			}
+		}
+		throw new IllegalArgumentException("No enum constant id=" + id);
+	}
+
+}
---
+++ src/test/java/com/github/hiuchida/api/consts/OrdTypeCodeTest.java
@@ -0,0 +1,39 @@
+package com.github.hiuchida.api.consts;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class OrdTypeCodeTest {
+
+	@Test
+	public void intValueTest() {
+		assertEquals(1, OrdTypeCode.ザラバ.intValue());
+		assertEquals(2, OrdTypeCode.寄り.intValue());
+		assertEquals(3, OrdTypeCode.引け.intValue());
+		assertEquals(4, OrdTypeCode.不成.intValue());
+		assertEquals(5, OrdTypeCode.対当指値.intValue());
+		assertEquals(6, OrdTypeCode.IOC.intValue());
+	}
+
+	@Test
+	public void toStringTest() {
+		assertEquals("1", OrdTypeCode.ザラバ.toString());
+		assertEquals("2", OrdTypeCode.寄り.toString());
+		assertEquals("3", OrdTypeCode.引け.toString());
+		assertEquals("4", OrdTypeCode.不成.toString());
+		assertEquals("5", OrdTypeCode.対当指値.toString());
+		assertEquals("6", OrdTypeCode.IOC.toString());
+	}
+
+	@Test
+	public void valueOfTest() {
+		assertEquals(OrdTypeCode.ザラバ, OrdTypeCode.valueOf(1));
+		assertEquals(OrdTypeCode.寄り, OrdTypeCode.valueOf(2));
+		assertEquals(OrdTypeCode.引け, OrdTypeCode.valueOf(3));
+		assertEquals(OrdTypeCode.不成, OrdTypeCode.valueOf(4));
+		assertEquals(OrdTypeCode.対当指値, OrdTypeCode.valueOf(5));
+		assertEquals(OrdTypeCode.IOC, OrdTypeCode.valueOf(6));
+	}
+
+}
