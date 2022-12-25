19846063672fe359f9e50284e72a53b3a07c1a93
hiuchida <hiro4uchida@gmail.com>
2022-12-25 18:29:06 +0900

    ExchangeCode.SOR追加

--- src/main/java/com/github/hiuchida/api/consts/ExchangeCode.java
+++ src/main/java/com/github/hiuchida/api/consts/ExchangeCode.java
@@ -5,7 +5,7 @@ package com.github.hiuchida.api.consts;
  * @see com.github.hiuchida.api.InfoApiWrapper symbol:銘柄コード（[銘柄コード]@[市場コード]）
  */
 public enum ExchangeCode {
-	東証(1), 名証(3), 福証(5), 札証(6), 日通し(2), 日中(23), 夜間(24);
+	東証(1), 名証(3), 福証(5), 札証(6), SOR(9), 日通し(2), 日中(23), 夜間(24);
 
 	private int id;
 
--- src/test/java/com/github/hiuchida/api/consts/ExchangeCodeTest.java
+++ src/test/java/com/github/hiuchida/api/consts/ExchangeCodeTest.java
@@ -12,6 +12,7 @@ public class ExchangeCodeTest {
 		assertEquals(3, ExchangeCode.名証.intValue());
 		assertEquals(5, ExchangeCode.福証.intValue());
 		assertEquals(6, ExchangeCode.札証.intValue());
+		assertEquals(9, ExchangeCode.SOR.intValue());
 		assertEquals(2, ExchangeCode.日通し.intValue());
 		assertEquals(23, ExchangeCode.日中.intValue());
 		assertEquals(24, ExchangeCode.夜間.intValue());
@@ -23,6 +24,7 @@ public class ExchangeCodeTest {
 		assertEquals("3", ExchangeCode.名証.toString());
 		assertEquals("5", ExchangeCode.福証.toString());
 		assertEquals("6", ExchangeCode.札証.toString());
+		assertEquals("9", ExchangeCode.SOR.toString());
 		assertEquals("2", ExchangeCode.日通し.toString());
 		assertEquals("23", ExchangeCode.日中.toString());
 		assertEquals("24", ExchangeCode.夜間.toString());
@@ -34,6 +36,7 @@ public class ExchangeCodeTest {
 		assertEquals(ExchangeCode.名証, ExchangeCode.valueOf(3));
 		assertEquals(ExchangeCode.福証, ExchangeCode.valueOf(5));
 		assertEquals(ExchangeCode.札証, ExchangeCode.valueOf(6));
+		assertEquals(ExchangeCode.SOR, ExchangeCode.valueOf(9));
 		assertEquals(ExchangeCode.日通し, ExchangeCode.valueOf(2));
 		assertEquals(ExchangeCode.日中, ExchangeCode.valueOf(23));
 		assertEquals(ExchangeCode.夜間, ExchangeCode.valueOf(24));
